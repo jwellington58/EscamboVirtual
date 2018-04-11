af7d6cdcb67c5a6ddf2c8c328bc2e8304c2443bc
jwellington58 <wj070996@gmail.com>
2018-04-11 16:56:50 -0300

    Adição do ENtity e das migrations

---
+++ EscamboVirtual/Infra.Data/Context/DataContext.cs
@@ -0,0 +1,48 @@
+﻿using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Infra.Data.Context
+{
+    public class DataContext : DbContext
+    {
+        public IConfigurationRoot Configuration { get; set; }
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        { }
+
+        public DataContext()
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // get the configuration from the app settings
+
+            var config = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+            // define the database to use
+            optionsBuilder.UseSqlServer(config.GetConnectionString("DefaultConnection"));
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+        public DbSet<Autor> Autores { get; set; }
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Endereco> Enderecos { get; set; }
+        public DbSet<Exemplar> Exemplares { get; set; }
+        public DbSet<Livro> Livros { get; set; }
+        public DbSet<LivroAutor> LivrosAutores { get; set; }
+        public DbSet<Pessoa> Pessoas { get; set; }
+        public DbSet<Troca> Trocas { get; set; }
+        public DbSet<Usuario> Usuarios { get; set; }
+    }
+}
---
+++ EscamboVirtual/Infra.Data/Migrations/20180411195506_FirtMigration.cs
@@ -0,0 +1,259 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace Infra.Data.Migrations
+{
+    public partial class FirtMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categorias",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Descricao = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categorias", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Enderecos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Cidade = table.Column<string>(nullable: true),
+                    Pais = table.Column<string>(nullable: true),
+                    Rua = table.Column<string>(nullable: true),
+                    Uf = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Enderecos", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Trocas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Trocas", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Livros",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    CategoriaId = table.Column<int>(nullable: false),
+                    Isbn = table.Column<string>(nullable: true),
+                    Nome = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Livros", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Livros_Categorias_CategoriaId",
+                        column: x => x.CategoriaId,
+                        principalTable: "Categorias",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Pessoas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    EnderecoId = table.Column<int>(nullable: false),
+                    Nome = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pessoas", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Pessoas_Enderecos_EnderecoId",
+                        column: x => x.EnderecoId,
+                        principalTable: "Enderecos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Autores",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    PessoaId = table.Column<int>(nullable: false),
+                    Resumo_Biografia = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Autores", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Autores_Pessoas_PessoaId",
+                        column: x => x.PessoaId,
+                        principalTable: "Pessoas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Exemplares",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    AnoCompra = table.Column<int>(nullable: false),
+                    LivroId = table.Column<int>(nullable: false),
+                    PessoaID = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Exemplares", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Exemplares_Livros_LivroId",
+                        column: x => x.LivroId,
+                        principalTable: "Livros",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Exemplares_Pessoas_PessoaID",
+                        column: x => x.PessoaID,
+                        principalTable: "Pessoas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Usuarios",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    PessoaId = table.Column<int>(nullable: false),
+                    Senha = table.Column<string>(nullable: true),
+                    UserName = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Usuarios", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Usuarios_Pessoas_PessoaId",
+                        column: x => x.PessoaId,
+                        principalTable: "Pessoas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "LivrosAutores",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    AutorId = table.Column<int>(nullable: false),
+                    LivroId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LivrosAutores", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_LivrosAutores_Autores_AutorId",
+                        column: x => x.AutorId,
+                        principalTable: "Autores",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_LivrosAutores_Livros_LivroId",
+                        column: x => x.LivroId,
+                        principalTable: "Livros",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Autores_PessoaId",
+                table: "Autores",
+                column: "PessoaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Exemplares_LivroId",
+                table: "Exemplares",
+                column: "LivroId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Exemplares_PessoaID",
+                table: "Exemplares",
+                column: "PessoaID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Livros_CategoriaId",
+                table: "Livros",
+                column: "CategoriaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LivrosAutores_AutorId",
+                table: "LivrosAutores",
+                column: "AutorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LivrosAutores_LivroId",
+                table: "LivrosAutores",
+                column: "LivroId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Pessoas_EnderecoId",
+                table: "Pessoas",
+                column: "EnderecoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Usuarios_PessoaId",
+                table: "Usuarios",
+                column: "PessoaId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Exemplares");
+
+            migrationBuilder.DropTable(
+                name: "LivrosAutores");
+
+            migrationBuilder.DropTable(
+                name: "Trocas");
+
+            migrationBuilder.DropTable(
+                name: "Usuarios");
+
+            migrationBuilder.DropTable(
+                name: "Autores");
+
+            migrationBuilder.DropTable(
+                name: "Livros");
+
+            migrationBuilder.DropTable(
+                name: "Pessoas");
+
+            migrationBuilder.DropTable(
+                name: "Categorias");
+
+            migrationBuilder.DropTable(
+                name: "Enderecos");
+        }
+    }
+}
