9e6244c071ecd94f78895509c34cac87623a5d65
jwellington58 <wj070996@gmail.com>
2018-04-11 13:04:09 -0300

    Adição da estrutura inicial DDD

---
+++ EscamboVirtual/Infra.CrossCutting.IoC/NativeIoC.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infra.CrossCutting.IoC
+{
+    public class NativeIoC
+    {
+
+        public static void RegisterServices(IServiceCollection services)
+        {
+
+        }
+
+    }
+}
